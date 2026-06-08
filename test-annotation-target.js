949552948a2dd6b30454ae3353a40aacddcd5137
Prabhu Subramanian <prabhu@appthreat.com>
2026-06-09 00:54:34 +0530

    Add vulnerable eval for annotation testing

--- test-annotation-target.js
+++ test-annotation-target.js
@@ -2,3 +2,7 @@
 const exec = require('child_process').exec;
 // TODO: fix SQL injection
 const query = 'SELECT * FROM users WHERE id = ' + req.params.id;
+
+// Vulnerable eval usage
+const userInput = req.body.code;
+eval(userInput);
